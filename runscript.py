781049613b79ca44341d94624803df563c537a56
Doroteya Stoyanova <64807351+doro041@users.noreply.github.com>
2025-03-09 22:57:42 +0000

    add dataloaders

--- runscript.py
+++ runscript.py
@@ -1,6 +1,12 @@
 from ultralytics import YOLO
 
+# Load the YOLO model
 model = YOLO("yolo12n.pt")
 
 
-model.train(data="dataset.yaml", epochs=100, imgsz=640)
+model.train(
+    data="dataset.yaml",
+    epochs=100,
+    imgsz=640,
+    workers=4 
+)
