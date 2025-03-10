bc79377b30568484411a27616d59d668c6d43daa
Doroteya Stoyanova <64807351+doro041@users.noreply.github.com>
2025-03-10 10:25:02 +0000

    Updated runbash.sh

--- runbash.sh
+++ runbash.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --nodes=1  number of nodes
+#SBATCH --nodes=4  number of nodes
 #SBATCH --cpus-per-task=12  number of cores
 #SBATCH --mem=32G  memory pool for all cores
 
 #SBATCH --ntasks-per-node=1  one job per node
-#SBATCH --gres=gpu:7  7 of the 21 paritions
+#SBATCH --gres=gpu:10  7 of the 21 paritions
 #SBATCH --partition=gpu
 
 #SBATCH -o slurm.%j.out  STDOUT
