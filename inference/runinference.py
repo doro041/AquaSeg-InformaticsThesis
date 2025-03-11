31597e6d09cd5033fc154907baa73bde0731a5db
doro041 <u17ds20@abdn.ac.uk>
2025-03-11 13:45:24 +0000

    run inference and saved models in a separate dir

---
+++ inference/runinference.py
@@ -0,0 +1,28 @@
+import os
+from ultralytics import YOLO
+
+
+model = YOLO('yolo11n_lobster.pt')
+
+source_folder = r'C:\Users\dorot\Desktop\Dissertation2025\MobileDevelopment\LobsterDataset2025\Aquaseg_Lobster_Dataset\MaleLobsters'
+output_folder = 'yolo11outputboxes'
+
+os.makedirs(output_folder, exist_ok=True)
+
+
+for image_name in os.listdir(source_folder):
+    if image_name.lower().endswith(('.png', '.jpg', '.jpeg')):
+        image_path = os.path.join(source_folder, image_name)
+        
+       
+        results = model(
+            source=image_path, 
+            conf=0.3, 
+            save=True,  
+            save_dir=output_folder,
+            show=False, 
+            show_labels=True,
+            show_conf=True
+        )
+
+print("Inference complete. Results saved to:", output_folder)
